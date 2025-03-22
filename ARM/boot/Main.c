034971b0d4a7ab23d39ddf3b20c50cbb3e07c404
nugongja <nugongja@gmail.com>
2025-03-22 21:33:01 +0900

    chapter5-간단한printf구현

--- ARM/boot/Main.c
+++ ARM/boot/Main.c
@@ -3,8 +3,10 @@
 #include "stdio.h"
 
 static void Hw_init(void);
+static void Printf_test(void);
 
-int main(void){
+
+void main(void){
 	Hw_init();
 
 	uint32_t i = 100;
@@ -15,6 +17,7 @@ int main(void){
 
 	putstr("Hello World!\n");
 
+	Printf_test();
 
 	i = 100;
 	while(i--){
@@ -27,3 +30,19 @@ static void Hw_init(void)
 {
 	Hal_uart_init();
 }
+
+
+static void Printf_test(void)
+{
+	char* str = "printf pointer test";
+	char* nullptr = 0;
+	uint32_t i = 5;
+
+	debug_printf("%s\n", "Hello printf");
+	debug_printf("output string pointer: %s\n", str);
+	debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+	debug_printf("%u = 5\n", i);
+	debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+	debug_printf("print zero %u\n", 0);
+}
+
