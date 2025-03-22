e0eee58eae9707df2561409109c452ab448694df
nugongja <nugongja@gmail.com>
2025-03-22 19:04:57 +0900

    chapter5-UART입력테스트

--- ARM/boot/Main.c
+++ ARM/boot/Main.c
@@ -4,7 +4,7 @@
 
 static void Hw_init(void);
 
-void main(void){
+int main(void){
 	Hw_init();
 
 	uint32_t i = 100;
@@ -13,7 +13,14 @@ void main(void){
 	}
 	Hal_uart_put_char('\n');
 
-	putstr("Hello Wolrd!\n");
+	putstr("Hello World!\n");
+
+
+	i = 100;
+	while(i--){
+		uint8_t ch = Hal_uart_get_char();
+		Hal_uart_put_char(ch);
+	}
 }
 
 static void Hw_init(void)
